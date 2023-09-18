040d02c053e6b83d79247e36bfe5d40f6c2e53ac
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-09-19 02:05:46 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your code here
+
+const name = document.getElementById("item-name-input");
+const price = document.getElementById("item-price-input");
+const table = document.getElementById("myTable")
+const total = document.getElementById('total');
+
+let i = 0;
+let priceArray=0;
+function handleClick() {
+    priceArray += parseInt(price.value);
+    table.innerHTML += `
+        <tr>
+            <td>${i + 1}</td>
+            <td>${name.value}</td>
+            <td>${price.value}</td>
+        </tr>
+    `
+    name.value = "";
+    price.value = "";
+    total.innerText = priceArray;
+    i++;
+}
